991a34ef677a8b1601dbb9ec8ef8415a3789fd33
Dev.MK <dev-mk@Dev.Mk>
2025-10-26 18:58:30 +0300

    Server config fix

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.use(helmet());
 app.use(cors({
   origin: config.cors.origins,
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Body parser
